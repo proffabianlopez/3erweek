737bf214adce0a4626d8486eccf651a40ea45918
Axel0rellana <axlto@hotmail.com>
2025-04-09 13:08:16 -0300

    Correcciones en clase

--- Autores.php
+++ Autores.php
@@ -1,47 +1,50 @@
 <?php
-function generar_pivote($archivo_entrada, $archivo_salida, $indice_campo)
+function generar_tabla_pivote($archivo_entrada, $archivo_salida, $indice_campo)
 {
-  $file = fopen($archivo_entrada, "r");
-  if (!$file) {
+  $archivo = fopen($archivo_entrada, "r");
+  if (!$archivo) {
     die("No se pudo abrir el archivo.");
   }
   
   $valores_unicos = [];
   
-  while (($line = fgets($file)) !== false) {
-    $fields = explode("|", trim($line));
-    if (isset($fields[$indice_campo])) {
-      $valor = trim($fields[$indice_campo]);
-
+  while (($linea = fgets($archivo)) !== false) {
+    $campos = explode("|", trim($linea));
+    
+    if (count($campos) < 7) {
+      continue;
+    }
+    
+    if (isset($campos[$indice_campo])) {
+      $valor = trim($campos[$indice_campo]);
       if ($valor === '') {
         $valor = "SIN DATOS";
       } else {
         $valor = strtoupper($valor);
       }
-      $valores_unicos[$valor] = true;
+      
+      if (!isset($valores_unicos[$valor])) {
+        $valores_unicos[$valor] = count($valores_unicos) + 1;
+      }
     } else {
-      $valores_unicos["SIN DATOS"] = true;
+      if (!isset($valores_unicos["SIN DATOS"])) {
+        $valores_unicos["SIN DATOS"] = count($valores_unicos) + 1;
+      }
     }
   }
-  fclose($file);
+  fclose($archivo);
   
-  $valores = array_keys($valores_unicos);
-  sort($valores);
-  
-  $pivot_file = fopen($archivo_salida, "w");
-  if (!$pivot_file) {
+  $archivo_pivote = fopen($archivo_salida, "w");
+  if (!$archivo_pivote) {
     die("No se pudo abrir el archivo de salida.");
   }
   
-  $id = 1;
-  
-  foreach ($valores as $valor) {
-    fwrite($pivot_file, $id . "|" . $valor . PHP_EOL);
-    $id++;
+  foreach ($valores_unicos as $valor => $id) {
+    fwrite($archivo_pivote, str_pad($id, 7, "0", STR_PAD_LEFT) . "|" . $valor . PHP_EOL);
   }
   
-  fclose($pivot_file);
+  fclose($archivo_pivote);
 }
 
-generar_pivote("Data/datos.txt", "Data/Autores.Dat", 4);
+generar_tabla_pivote("Data/datos.txt", "Data/Autores.Dat", 4);
 echo "\033[32mArchivo Autores.Dat creado exitosamente\033[0m";
